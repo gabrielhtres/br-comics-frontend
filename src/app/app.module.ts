950f5090ac7b691eec9330e8001fe63bf4ea4df2
Gabriel <gabrielhtres@gmail.com>
2022-05-12 15:17:56 -0300

    Criado rodape e alterado logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { SliderTopoComponent } from './slider-topo/slider-topo.component';
 import { SliderEditorasComponent } from './slider-editoras/slider-editoras.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     SliderTopoComponent,
-    SliderEditorasComponent
+    SliderEditorasComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
