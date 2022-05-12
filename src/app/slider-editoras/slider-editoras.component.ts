d729b7a9ba15e90d82a7571fadd6e5c85bbae32b
Gabriel <gabrielhtres@gmail.com>
2022-05-12 14:27:06 -0300

    Ajustes na estilização do slider de editoras da home

--- src/app/slider-editoras/slider-editoras.component.ts
+++ src/app/slider-editoras/slider-editoras.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider-editoras.component.scss']
 })
 export class SliderEditorasComponent implements OnInit {
+  public hqs = [
+    { img: '../../assets/Capa HQ.png', titulo: 'Batman - O Cavaleiro das Trevas' },
+    { img: '../../assets/Capa HQ.png', titulo: 'Batman' },
+    { img: '../../assets/Capa HQ.png', titulo: 'Batman' },
+    { img: '../../assets/Capa HQ.png', titulo: 'Batman' },
+    { img: '../../assets/Capa HQ.png', titulo: 'Batman' },
+    { img: '../../assets/cav-lua.jpg', titulo: 'Cavaleiro da Lua #10' }
+  ]
+
+  public editoras = [
+    { editora: 'DC Comics' },
+    { editora: 'Marvel Comics' },
+  ]
 
   constructor() { }
 
